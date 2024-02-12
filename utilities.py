97f2d441f7db05800b8ae82052815dbb5be4d350
jrizxos <jrizos98@gmail.com>
2024-02-12 13:47:06 +0200

    Update utilities.py
    Small change, removed some unused methods to avoid confusion.

--- utilities.py
+++ utilities.py
@@ -3,69 +3,8 @@ import numpy as np
 import pandas as pd
 from typing import List
 from bitarray import bitarray
-from score_tree import load_raw, load_tree, find_individual
-from search import VAR_N, BIT_W, get_ints, individual, EXACT, UNITS
-
-def convert_64_to_vals(s:str):
-    """Converts a base64 Individual name to the list of corresponding variable values
-
-        Args:
-            s (str): base64 encoded str
-
-        Returns:
-            vals (list of ints): the variable values
-    """
-    vals = []
-    dec_bytes = base64.standard_b64decode(s)
-    enc_bits = bitarray(endian='big')               # get a bitarray object
-    enc_bits.frombytes(dec_bytes)                   # get bitarray from bytes
-    enc_str = enc_bits.to01()                       # get str of bits
-    enc_str = enc_str[len(enc_str)-BIT_W*VAR_N:]
-    vals = get_ints(enc_str)
-    return vals
-
-def convert_64_to_16(s:str):
-    """Converts a base64 Individual name to the corresponding base16 Individual name
-
-        Args:
-            s (str): base64 encoded str
-
-        Returns:
-            (str): corresponding base16 str
-    """
-    vals = convert_64_to_vals(s)
-    ind = individual(vals)
-    return ind.loaded_id
-
-def convert_16_to_vals(s:str):
-    dec_int = int(s, base=16)
-    enc_bits = bitarray(endian='big')
-    enc_bits.frombytes(dec_int.to_bytes(length=25,byteorder='big'))
-    enc_str = enc_bits.to01()
-    enc_str = enc_str[len(enc_str)-BIT_W*VAR_N:]
-    vals = get_ints(enc_str)
-    return vals
-
-def convert_16_to_64(s:str):
-    """Converts a base16 Individual name to the corresponding base64 Individual name
-
-        Args:
-            s (str): base16 encoded str
-
-        Returns:
-            (str): corresponding base64 str
-    """
-    vals = convert_16_to_vals(s)
-    ind = individual(vals)
-    return ind.shorter_name()
-
-def convert_int_to_vals(i:int):
-    enc_bits = bitarray(endian='big')
-    enc_bits.frombytes(i.to_bytes(length=25,byteorder='big'))
-    enc_str = enc_bits.to01()
-    enc_str = enc_str[len(enc_str)-BIT_W*VAR_N:]
-    vals = get_ints(enc_str)
-    return vals
+from score_tree import load_raw
+from search import VAR_N, BIT_W, individual, EXACT, UNITS
 
 def get_dataframe(highscores:List[individual]):
     """Creates a pandas dataframe from an idividual database dictionary.
